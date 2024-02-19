03241f4071d45daf9b1867fd5e83bdc5462dd7a4
bwang1008 <bwang1008@gmail.com>
2024-02-19 14:53:07 -0600

    Finished Your Ride Is Here

--- chapter1/section2/README.md
+++ chapter1/section2/README.md
@@ -1,1 +1,2 @@
 * Submitting Solutions: previously submitted on 2017-07-22. Last submitted on 2024-02-19.
+* Your Ride Is Here: previously submitted on 2017-07-22. Last submitted on 2024-02-19. This was submission #8.
---
+++ chapter1/section2/your_ride_is_here/ride.cpp
@@ -0,0 +1,37 @@
+/*
+ID: bwang101
+TASK: ride
+LANG: C++14
+*/
+
+#include <fstream>
+#include <iostream>
+
+int convert_name_to_int(const std::string s) {
+	int product = 1;
+	for(const char c : s) {
+		product = (product * (c - 'A' + 1)) % 47;
+	}
+
+	return product;
+}
+
+int main() {
+	const std::string file_base_name = "ride";
+	std::ifstream fin(file_base_name + ".in");
+	std::ofstream fout(file_base_name + ".out");
+
+	std::string comet_name;
+	std::string group_name;
+
+	fin >> comet_name >> group_name;
+
+	const std::string output_message =
+		(convert_name_to_int(comet_name) == convert_name_to_int(group_name))
+			? "GO"
+			: "STAY";
+
+	fout << output_message << std::endl;
+
+	return 0;
+}
