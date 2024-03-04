d5dd51463eded37263b96b2aa5f1daf893d5e608
bwang1008 <bwang1008@gmail.com>
2024-03-03 19:39:32 -0600

    Refactor Wormholes

--- chapter1/section4/wormholes/wormhole.cpp
+++ chapter1/section4/wormholes/wormhole.cpp
@@ -7,75 +7,28 @@ LANG: C++14
 #include <cstddef>	 // std::size_t
 #include <fstream>	 // std::ifstream, std::ofstream
 #include <iostream>	 // std::endl
-#include <set>		 // std::set
 #include <string>	 // std::string
 #include <utility>	 // std::pair, std::make_pair
 #include <vector>	 // std::vector
 
-void print_vec(const std::vector<int>& vec) {
-	std::cout << "[";
-	for(std::size_t i = 0; i < vec.size(); ++i) {
-		std::cout << vec[i];
-		if(i + 1 < vec.size()) {
-			std::cout << ", ";
+auto portal_pairs_has_infinite_loop(const std::vector<int>& portal_links,
+									const std::vector<int>& right_portal_index)
+	-> bool {
+	const std::size_t N = portal_links.size();
+	for(std::size_t start = 0; start < N; ++start) {
+		// try each portal as a starting point of Bessie
+		int current_portal_index = start;
+		// attempt to go right through N portals: go through portal first, then
+		// move right
+		for(std::size_t i = 0; i < N; ++i) {
+			if(current_portal_index == -1) {
+				break;
+			}
+			current_portal_index =
+				right_portal_index[portal_links[current_portal_index]];
 		}
-	}
-	std::cout << "]" << std::endl;
-}
 
-auto next_coord(const std::vector<int>& portal_links,
-				const std::vector<std::pair<int, int>>& portal_coords,
-				const std::pair<int, int> curr_coord,
-				const std::set<int>& xs) -> std::pair<int, int> {
-	// check if at entrance to portal: if so, go through portal, and move right
-	// 1
-	for(std::size_t i = 0; i < portal_coords.size(); ++i) {
-		const std::pair<int, int> candidate_portal_coord = portal_coords[i];
-		if(candidate_portal_coord == curr_coord) {
-			const int linked_portal_index = portal_links[i];
-			const std::pair<int, int> linked_portal_coord =
-				portal_coords[linked_portal_index];
-			return {1 + linked_portal_coord.first, linked_portal_coord.second};
-		}
-	}
-
-	// move to next x coord
-	const std::set<int>::iterator next_it = xs.upper_bound(curr_coord.first);
-	if(next_it == xs.end()) {
-		return {1 + curr_coord.first, curr_coord.second};
-	}
-
-	return {*next_it, curr_coord.second};
-}
-
-auto portal_pairs_from_start_has_infinite_loop(
-	const std::vector<int>& portal_links,
-	const std::vector<std::pair<int, int>>& portal_coords,
-	const std::pair<int, int> start_coords,
-	const std::set<int>& xs) -> bool {
-	const std::size_t N = portal_coords.size();
-
-	const std::size_t max_num_moves = 2 * N;
-
-	std::set<std::pair<int, int>> visited_coords;
-	std::pair<int, int> curr_coords = start_coords;
-
-	for(std::size_t move_index = 0; move_index < max_num_moves; ++move_index) {
-		curr_coords = next_coord(portal_links, portal_coords, curr_coords, xs);
-		visited_coords.insert(curr_coords);
-	}
-
-	return visited_coords.size() < max_num_moves;
-}
-
-auto portal_pairs_has_infinite_loop(
-	const std::vector<int>& portal_links,
-	const std::vector<std::pair<int, int>>& portal_coords,
-	const std::set<int>& xs) -> bool {
-	for(const std::pair<int, int> portal_coord : portal_coords) {
-		const bool result = portal_pairs_from_start_has_infinite_loop(
-			portal_links, portal_coords, portal_coord, xs);
-		if(result) {
+		if(current_portal_index != -1) {
 			return true;
 		}
 	}
@@ -129,20 +82,35 @@ auto main() -> int {
 	fin >> N;
 
 	std::vector<std::pair<int, int>> portal_coords;
-	std::set<int> xs;
 	for(std::size_t i = 0; i < N; ++i) {
 		int a = 0;
 		int b = 0;
 		fin >> a >> b;
 		portal_coords.emplace_back(a, b);
-		xs.insert(a);
+	}
+
+	// right_portal_index[i] is the index of the portal that is to the immediate
+	// right of portal i
+	std::vector<int> right_portal_index(N, -1);
+	for(std::size_t i = 0; i < N; ++i) {
+		for(std::size_t j = 0; j < N; ++j) {
+			// update right_portal_index[i], if j has same y-coordinate, and
+			// it's either new or is closer than previous candidate
+			if(portal_coords[i].first < portal_coords[j].first &&
+			   portal_coords[i].second == portal_coords[j].second &&
+			   (right_portal_index[i] == -1 ||
+				portal_coords[j].first <
+					portal_coords[right_portal_index[i]].first)) {
+				right_portal_index[i] = j;
+			}
+		}
 	}
 
 	const std::vector<std::vector<int>> combos = find_pair_combinations(N);
 
 	int total = 0;
 	for(const std::vector<int>& portal_links : combos) {
-		if(portal_pairs_has_infinite_loop(portal_links, portal_coords, xs)) {
+		if(portal_pairs_has_infinite_loop(portal_links, right_portal_index)) {
 			total++;
 		}
 	}
